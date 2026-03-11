4c8342e2bbd47b0937a5e6e1d4aab65eade8368b
samcassdev <samcasseusdev@gmail.com>
2026-03-10 21:10:28 -0500

    Stacks Neetcode Challenges

---
+++ evaluateReversePolishNotation/solution.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = [];
+
+        const operators = {
+            '+': (num1, num2) => num1 + num2,
+            '-': (num1, num2) => num1 - num2,
+            '*': (num1, num2) => num1 * num2,
+            '/': (num1, num2) => Math.trunc(num1 / num2)
+        };
+        for(let token of tokens){
+            if(operators[token]){
+                let num2 = stack.pop();
+                let num1 = stack.pop();
+
+                stack.push(operators[token](num1, num2))
+            } else {
+                stack.push(parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+};
---
+++ largestRectangleInHistogram/README.md
@@ -0,0 +1,20 @@
+You are given an array of integers heights where heights[i] represents the height of a bar. The width of each bar is 1.
+
+Return the area of the largest rectangle that can be formed among the bars.
+
+Note: This chart is known as a histogram.
+
+Example 1:
+
+Input: heights = [7,1,7,2,2,4]
+
+Output: 8
+Example 2:
+
+Input: heights = [1,3,7]
+
+Output: 7
+Constraints:
+
+1 <= heights.length <= 1000.
+0 <= heights[i] <= 1000
